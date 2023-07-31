c86d286ec27428ac51f24fdfcafde346e7f35540
ido rabin <idorabin@idos-MacBook-Pro.local>
2023-08-01 02:32:35 +0300

    config docker file for psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+calc functions
+"""
+def add(x,y):
+    """add x and y and return result"""
+    return x+y
+def sub(x,y):
+    return x-y
---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+sample test
+"""
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+        
+    def test_sub_numbers(self):
+        res = calc.sub(6,5)
+        self.assertEqual(res,1)
