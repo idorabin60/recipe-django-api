e2aa736b15cf1b6c8316af633b8506006bf62786
ido rabin <idorabin@idos-MacBook-Pro.local>
2023-08-02 17:09:04 +0300

    test

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,10 +4,12 @@ from psycopg2 import OperationalError as Psycopg2Error
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
+
 class Command(BaseCommand):
     """Django command to w8 for db"""
 
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         """Entrypoing for command"""
         self.stdout.write('Waiting for db...')
         db_up = False
@@ -18,6 +20,5 @@ class Command(BaseCommand):
             except:
                 self.stdout.write('db is unavailleble waiting 1 sec...')
                 time.sleep(1)
-                
+
         self.stdout.write(self.style.SUCCESS('DataBase abailable !'))
-        
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        BaseUserManager,
+                                        PermissionsMixin,)
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    """manager for users"""
+
+    def create_user(self, email, passowrd=None, **extra_field):
+        """Create,save and return new user"""
+        user = self.model(email=email, **extra_field)
+        user.set_password(passowrd)
+        user.save(using=self._db)
+        return user
+
+
+class User (AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager
+
+    USERNAME_FIELD = 'email'
